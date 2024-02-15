99f2f3cfc1f3bedd7d6a3ef7eb014003aa9425c1
C-ON <vmfpsel@gmail.com>
2024-02-15 16:01:38 +0900

    setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+/**
+ * class --> className
+ * 데이터 바인딩 : {}
+ * useState : 자료 저장 
+ */
 
 function App() {
+  
+  let post = '강남 우동 맛집';
+  let [title, b] = useState('남자 코트 추천');
+
+  // Destructuring 문법
+  let num = [1, 2];
+  // let a = num[0];
+  // let c= num[1];
+  let [a, c] = [1, 2];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>Blog</h4>
+      </div>
+      <div className='list'>
+        <h4>글 제목</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{ title }</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
